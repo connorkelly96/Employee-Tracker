14f0f261cdd859b8fd68954681e4d5e43bd1adf9
connork96 <connorkelly96@yahoo.com>
2022-10-04 20:55:01 -0400

    added code to db/index to create and change the data in the tables

--- db/index.js
+++ db/index.js
@@ -1,5 +1,49 @@
 const connection = require("./connection");
 
 class employeeDB {
+    constructor(connection) {
+        this.connection = connection;
+    }
 
+    // Find all the employees
+    allEmployees() {
+        return this.connection.promise().query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+        );
+    }
+
+    // Add employee
+    addEmployee(employee) {
+        return this.connection.promise().query("INSERT INTO employee SET ?", employee);
+    }
+
+    // Delete employee
+    deleteEmployee(employeeId) {
+        return this.connection.promise().query(
+            "DELETE FROM employee WHERE id = ?",
+            employeeId
+        );
+    }
+
+    // Find all the managers
+    allManagers(employeeId) {
+        return this.connection.promise().query(
+            "SELECT id, first_name, last_name FROM employee WHERE id != ?",
+            employeeId
+        );
+    }
+
+    // Find all the roles
+    allRoles() {
+        return this.connection.promise().query(
+            "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+        );
+    }
+
+    // Create new role
+    createRole(role) {
+        return this.connection.promise().query("INSERT INTO role SET ?", role);
+    }
+
+}
     module.exports = new employeeDB(connection);
